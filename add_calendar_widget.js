c39ec506f775a4394e0328fdf7d984a4b383563f
Joern Boegeholz <boegeholz.joern@gmail.com>
2019-04-03 11:05:17 +0200

    remove old GM API functions

--- add_calendar_widget.js
+++ add_calendar_widget.js
@@ -5,27 +5,12 @@
 // @include     <your site here>
 // @require https://code.jquery.com/jquery-1.12.4.min.js
 // @require https://code.jquery.com/ui/1.12.1/jquery-ui.min.js
-// @resource customCSS https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css
-// @resource IconSet1 https://code.jquery.com/ui/1.12.1/themes/base/images/ui-icons_444444_256x240.png
-// @resource IconSet2 https://code.jquery.com/ui/1.12.1/themes/base/images/ui-icons_555555_256x240.png
-// @version     1
-// @grant       GM_addStyle
-// @grant       GM_getResourceText
-// @grant       GM_getResourceURL
-
-
-// ==/UserScript==
-var iconSet1    = GM_getResourceURL ("IconSet1");
-var iconSet2    = GM_getResourceURL ("IconSet2");
-
-var newCSS = GM_getResourceText ("customCSS");
-newCSS     = newCSS.replace (/url\(images\/ui\-bg_.*00\.png\)/g, "");
-newCSS     = newCSS.replace (/images\/ui-icons_444444_256x240\.png/g, iconSet1);
-newCSS     = newCSS.replace (/images\/ui-icons_555555_256x240\.png/g, iconSet2);
-GM_addStyle (newCSS);
+// @version     2
 
 $(document).ready(function(){
-    console.log("script works");
-    $( 'input[name="von"]' ).datepicker({ firstDay: 1, dateFormat: 'dd.mm.yy' });
-    $( 'input[name="bis"]' ).datepicker({ firstDay: 1, dateFormat: 'dd.mm.yy' });
+  console.log("script works");
+  $( 'input[name="von"]' ).datepicker({ firstDay: 1, dateFormat: 'dd.mm.yy' });
+  $( 'input[name="bis"]' ).datepicker({ firstDay: 1, dateFormat: 'dd.mm.yy' });
+  $('#i-datepicker-div').css('background-color', '');
+  $("#ui-datepicker-div").css({'background-color':'white'});
 });
